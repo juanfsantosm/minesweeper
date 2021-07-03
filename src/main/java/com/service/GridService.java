cdaf1ff0fda08bb8ef9e96641f8afb30e2bc6f39
juan <juanfernando.santosm@gmail.com>
2021-07-03 07:33:04 -0500

    load the grid in order to be able to continue playing

--- src/main/java/com/service/GridService.java
+++ src/main/java/com/service/GridService.java
@@ -3,6 +3,7 @@ package com.service;
 import com.deviget.exception.BuildGridException;
 import com.deviget.model.Game;
 import com.deviget.model.Grid;
+import com.deviget.persistence.GameEntity;
 
 public interface GridService {
    
@@ -15,4 +16,6 @@ public interface GridService {
     void redFlagCellAt(Grid grid, int x, int y);
 
     Grid buildGrid(Game game , int rowCount , int columnCount, int minedCells) throws BuildGridException;
+
+	Grid loadGrid(GameEntity entity);
 }
